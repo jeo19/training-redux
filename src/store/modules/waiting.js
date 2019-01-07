fcadf9e536970490a69e45cae27fec18ab33142f
nila <terrakkk@outlook.com>
2019-01-07 15:47:26 +0630

    Use the id as a unique value on createAction function

--- src/store/modules/waiting.js
+++ src/store/modules/waiting.js
@@ -1,9 +1,11 @@
-import { createAction } from 'redux-actions';
+import { createAction, handleActions } from 'redux-actions';
 const CHANGE_INPUT = 'waiting/CHANGE_INPUT';
 const CREATE = 'waiting/CREATE';
 const ENTER = 'waiting/ENTER';
 const LEAVE = 'waiting/LEAVE';
+let id = 0;
 export const changeInput = createAction(CHANGE_INPUT, text => text);
-export const create = createAction(CREATE, text => text);
+export const create = createAction(CREATE, text => ({ text, id: id++ }));
 export const enter = createAction(ENTER, id => id);
 export const leave = createAction(LEAVE, id => id);
+export default handleActions({});
