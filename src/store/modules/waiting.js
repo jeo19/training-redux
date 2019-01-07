3149fc7ab29c0e3000060a2b7d9423d2933defdc
nila <terrakkk@outlook.com>
2019-01-07 15:41:53 +0630

    Use the createAction funcion

--- src/store/modules/waiting.js
+++ src/store/modules/waiting.js
@@ -1,8 +1,9 @@
+import { createAction } from 'redux-actions';
 const CHANGE_INPUT = 'waiting/CHANGE_INPUT';
 const CREATE = 'waiting/CREATE';
 const ENTER = 'waiting/ENTER';
 const LEAVE = 'waiting/LEAVE';
-export const changeInput = text => ({ type: CHANGE_INPUT, payload: text });
-export const create = text => ({ type: CREATE, payload: text });
-export const enter = id => ({ type: ENTER, payload: id });
-export const leave = id => ({ type: LEAVE, payload: id });
+export const changeInput = createAction(CHANGE_INPUT, text => text);
+export const create = createAction(CREATE, text => text);
+export const enter = createAction(ENTER, id => id);
+export const leave = createAction(LEAVE, id => id);
